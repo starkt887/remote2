6adeae22750c33e171470429d7bf1a76f0a0be3a
Samar Pratap <starkt887@gmail.com>
2024-07-31 22:54:58 +0530

    modified files and coutner component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+type Props = {}
+
+const Counter = (props: Props) => {
+    const [count, setCount] = useState(0)
+    return (
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+        </div>
+    )
+}
+
+export default Counter
